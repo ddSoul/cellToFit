0484344a26d2a08e7031738d2b6d68ce805bc30d
dddxl <iosmrdeng@163.com>
2016-08-13 17:42:40 +0800

    1.优化cell里的处理

--- cellToFit/Classes/Views/CustomTableViewCell.m
+++ cellToFit/Classes/Views/CustomTableViewCell.m
@@ -30,7 +30,6 @@
     if (self)
     {
         [self createControls];
-        [self setAutoLayout];
     }
     return self;
 }
@@ -46,29 +45,21 @@
     [self.contentView addSubview:self.titleLabel];
     
 }
+
+- (void)setModel:(Contentlist *)model
+{
+    self.titleLabel.text = model.text;
+    [self setAutoLayout];
+}
+
 - (void)setAutoLayout
 {
     [self.titleLabel mas_makeConstraints:^(MASConstraintMaker *make){
         make.top.left.mas_equalTo(60*view_scal);
         make.right.bottom.mas_equalTo(-60*view_scal);
-        //        make.height.mas_equalTo(70*view_scal);
     }];
 }
-- (void)setModel:(Contentlist *)model
-{
-    self.titleLabel.text = model.text;
-    CGFloat height = [UILabel getHeightByWidth:ScreenWidth-120*view_scal title:model.text font:[UIFont systemFontOfSize:50*view_scal]];
-    [self upLayout:height];
-}
-- (void)upLayout:(CGFloat)height
-{
-    [self.titleLabel mas_updateConstraints:^(MASConstraintMaker *make){
-        make.height.mas_equalTo(height);
-        make.top.left.mas_equalTo(60*view_scal);
-        make.bottom.mas_equalTo(-60*view_scal);
-    }];
-    
-}
+
 
 
 - (void)awakeFromNib {
